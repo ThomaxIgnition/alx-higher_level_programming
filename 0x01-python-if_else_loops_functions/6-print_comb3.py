e2bcf846e7d2d39c46246a4483f0c880f5f6e67b
ThomaxIgnition <113600602+ThomaxIgnition@users.noreply.github.com>
2023-03-09 20:10:47 +0100

    Update 6-print_comb3.py

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,5 +1,6 @@
+
 #!/usr/bin/python3
-for i in range(0, 8):
-    for j in range(i + 1, 10):
-        print("{:d}{:d}".format(i, j), end=', ')
-print("{:d}{:d}".format(i + 1, j))
+str1 = "Holberton"
+str2 = "School"
+str1 += ' ' + str2
+print(f'Welcome to {str1}!')
